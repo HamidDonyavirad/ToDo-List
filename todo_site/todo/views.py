4ef95551e8de63744aa32b9bf84091dd5b0d0692
HamidDonyavirad <donyavirad76@gmail.com>
2025-09-05 21:50:46 +0330

    fix:fixing errors

--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import  TodoForm
+from .forms import  ToDoForm
 from .models import ToDo
 
 
 def index(request):
     item_list = ToDo.objects.order_by('-date')
     if request.method  == 'POST':
-        form = ToDo.form(request.POST)
+        form = ToDoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('todo')
-    form = TodoForm()
+    form = ToDoForm()
     page = {
         'forms': form,
         'list':item_list,
