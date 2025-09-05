e98a5132656b8168e6c2d8607ce2120c38df5acc
HamidDonyavirad <donyavirad76@gmail.com>
2025-09-05 21:05:36 +0330

    feat(views.py):creating views for project

--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import  TodoForm
+from .models import ToDo
 
-# Create your views here.
+
+def index(request):
+    item_list = ToDo.objects.order_by('-date')
+    if request.method  == 'POST':
+        form = ToDo.form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+    form = TodoForm()
+    page = {
+        'forms': form,
+        'list':item_list,
+        'title':'TODO LIST',
+    }
+    return render(request,'todo/index.html',page)
+
+
+def remove(request, item_id):
+    item = ToDo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, 'Item deleted')
+    return redirect('todo')
+
+
+
